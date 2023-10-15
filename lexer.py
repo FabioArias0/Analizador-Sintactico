055e75931795e64af4204bfe46224318727c6921
FabioArias0 <114270134+FabioArias0@users.noreply.github.com>
2023-10-15 19:02:03 -0400

    Codigo + Ejecutable del analizador sintactico utilizando Python

---
+++ lexer.py
@@ -0,0 +1,35 @@
+import ply.lex as lex
+
+# Lista de tokens
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+)
+
+# Definición de tokens
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Ignorar espacios y tabulaciones
+t_ignore = ' \t'
+
+# Aqui va a capturar los errores
+def t_error(t):
+    print(f"Carácter ilegal '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
---
+++ parser.py
@@ -0,0 +1,39 @@
+import ply.yacc as yacc
+from lexer import tokens
+
+def p_expression(p):
+    '''expression : expression PLUS term
+                  | expression MINUS term
+                  | term'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        if p[2] == '+':
+            p[0] = p[1] + p[3]
+        elif p[2] == '-':
+            p[0] = p[1] - p[3]
+
+def p_term(p):
+    '''term : term TIMES factor
+            | term DIVIDE factor
+            | factor'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        if p[2] == '*':
+            p[0] = p[1] * p[3]
+        elif p[2] == '/':
+            p[0] = p[1] / p[3]
+
+def p_factor(p):
+    '''factor : NUMBER
+              | LPAREN expression RPAREN'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = p[2]
+
+def p_error(p):
+    print("Error de sintaxis")
+
+parser = yacc.yacc()
